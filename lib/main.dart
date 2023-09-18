711c18d82d1f04b299a0267c753a730aeab25058
alibek.g <gadzhibaev@webant.ru>
2023-09-18 09:32:28 +0400

    add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rick_and_morty_app/presentation/cubits/planets/planets_cubit.dart';
+import 'package:rick_and_morty_app/presentation/router/app_router.dart';
 
 import 'injection/di.dart';
-import 'presentation/stars_background_drawer.dart';
-import 'presentation/warp_speed_drawer.dart';
+
+final _appRouter = AppRouter();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,35 +17,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BlocProvider(
-        create: (context) => PlanetsCubit(),
-        child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          constraints: BoxConstraints.expand(),
-          alignment: Alignment.center,
-          child: Stack(
-            children: [
-              StarsBackgroundDrawer(),
-              WarpSpeedDrawer(),
-              Builder(builder: (context) {
-                return MaterialButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    context.read<PlanetsCubit>().fetchAllPlanets();
-                  },
-                );
-              }),
-            ],
-          ),
-        ),
-      ),
+      routerConfig: _appRouter.config(),
     );
   }
 }
